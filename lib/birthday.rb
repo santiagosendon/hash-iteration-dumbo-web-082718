fe3d9c90ffae55c1e88b51f1fbf7b1fa23bcde15
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-21 00:17:35 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,33 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+ 
+  birthday_kids = {
+    "Timmy" => 9,
+    "Sarah" => 6,
+    "Amanda" => 27
+}
+
+ birthday_kids.each { |kids_name, age|
+ puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
 
 
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,18 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+ passengers = {
+suite_a: "Amanda Presley",
+suite_b: "Seymour Hoffman",
+suite_c: "Alfred Tennyson",
+suite_d: "Charlie Chaplin",
+suite_e: "Crumpet the Elf"
+} 
+  
+ winner = ""
+ passengers, each { |suite|, |name|}
+ if suite == :suite_a && name.start_with?("A")
+   winner = name
+ end
 end
+winner
